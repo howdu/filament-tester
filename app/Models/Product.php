b2c3ed75b4900dabc3e465786e8952b8110b12bb
Ben Johnson <howdu@users.noreply.github.com>
2022-12-19 09:33:02 +0000

    Product and Comments

---
+++ app/Models/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name'
+    ];
+
+    public function comments(): MorphToMany
+    {
+        return $this->morphToMany(Comment::class, 'commentable', 'commentables')
+            ->withPivot('ordered')->orderByRaw('ISNULL(commentables.ordered), commentables.ordered ASC');
+    }
+}

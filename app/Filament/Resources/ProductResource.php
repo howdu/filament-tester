4a57977a6ca284a9de8ed212b32a6320f9c25cf6
Ben Johnson <howdu@users.noreply.github.com>
2023-01-27 11:56:25 +0000

    Update with delete action bug

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -6,15 +6,11 @@ use App\Filament\Resources\ProductResource\Pages;
 use App\Filament\Resources\ProductResource\RelationManagers;
 use App\Models\Product;
 use Filament\Forms;
-use Filament\Notifications\Notification;
-use Filament\Pages\Actions\Action;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class ProductResource extends Resource
@@ -28,25 +24,6 @@ class ProductResource extends Resource
         return [
             Forms\Components\TextInput::make('name')
                 ->required(),
-            Forms\Components\Repeater::make('attributes')
-                ->relationship()
-                ->required()
-                ->columnSpanFull()
-                ->schema([
-                    Forms\Components\Grid::make(3)
-                        ->schema([
-                            Forms\Components\TextInput::make('key'),
-                            Forms\Components\TextInput::make('value'),
-                            Forms\Components\Select::make('group_id')
-                                ->relationship('group', 'name')
-                                ->preload()
-                                ->required()
-                                ->createOptionForm([
-                                    Forms\Components\TextInput::make('name')
-                                        ->required()
-                                ])
-                        ])
-                ])
         ];
     }
 
@@ -61,51 +38,17 @@ class ProductResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('name'),
-                Tables\Columns\TextColumn::make('attributes_count')
-                    ->label('Attributes')
-                    ->alignCenter(),
-                Tables\Columns\TextColumn::make('comments_count')
-                    ->label('Comments')
-                    ->alignCenter()
             ])
             ->filters([
                 //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\ReplicateAction::make()
-                    ->form(ProductResource::getFormSchema())
-                    ->action(function (Model $record, array $data, Tables\Actions\ReplicateAction $action, $form) {
-                        $replica = $record->replicate(['attributes_count', 'comments_count']);
-                        $replica->name = $data['name'];
-
-                        if (!$replica->save()) {
-                            return false;
-                        }
-
-                        // Manually save attributes relationship
-                        foreach($form->getComponents() as $component) {
-                            if (!$component instanceof Forms\Components\Repeater ||
-                                $component->getStatePath(false) !== 'attributes') {
-                                continue;
-                            }
-
-                            $childComponentContainers = $component->getChildComponentContainers();
-
-                            foreach ($childComponentContainers as $item) {
-                                $itemData = $item->getState(shouldCallHooksBefore: false);
-
-                                $replica->attributes()->create($itemData);
-                            }
-                        }
-
-                        Notification::make()
-                            ->title('Product replicated successfully')
-                            ->success()
-                            ->send();
-
-                        $action->redirect(ProductResource::getUrl('edit', $replica->getKey()));
-                    })
+                Tables\Actions\DeleteAction::make()
+                    ->before(function (\Filament\Pages\Actions\DeleteAction $action) {
+                        $record = $action->getRecord();
+                        dd($record);
+                    }),
             ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
@@ -115,17 +58,9 @@ class ProductResource extends Resource
     public static function getEloquentQuery(): Builder
     {
         return parent::getEloquentQuery()
-            ->withCount(['attributes', 'comments'])
             ->withoutGlobalScopes([
                 SoftDeletingScope::class,
             ]);
-    }
-
-    public static function getRelations(): array
-    {
-        return [
-            RelationManagers\CommentRelationManager::class
-        ];
     }
 
     public static function getPages(): array

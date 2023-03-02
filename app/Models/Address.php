d7584955b6d7883223a8ac30df3c5585d737b836
Ben Johnson <howdu@users.noreply.github.com>
2023-03-02 12:26:28 +0000

    Issue with repeater relationships

---
+++ app/Models/Address.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+}
